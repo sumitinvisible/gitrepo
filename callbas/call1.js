71e7d5dd76addbc87f067853c77f04d9813e20cf
sumit <sumitranchi003@gmail.com>
2018-11-23 11:06:14 +0530

    screen2

--- callbas/call1.js
+++ callbas/call1.js
@@ -5,6 +5,14 @@ return c;
 var res = add(78,98);
 console.log(res);
 
+setTimeout(()=>{
+
+console.log('Inside of callback');
+},2000)
 
 // for syncs call we only need call stack that is enough to execute the code 
+// settimeout is a node api as it is not availavle in v8 engine ,node provide this feature
+
+// refer figure for better visualization
+
 
